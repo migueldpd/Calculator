f54bc49ccd71e4bd01a56a313bdb5934ac629403
Migueldpd <miguelex06@gmail.com>
2024-03-24 09:10:00 +0000

    Improvements on layout and adding buttons

--- calc.js
+++ calc.js
@@ -1,8 +1,8 @@
 let currentValue = " "; //so any time a user inserts a number , the number adds instead of ghosting
-let impressNumber = document.getElementById("result");
+let impressNumber = document.getElementById("result"); //this is the value inside the input type text
 
 
-function resetCalculator(){
+function resetCalculator(){ //makes it so when this button is pressed it clears the input
     impressNumber.value= '';
     currentValue= ' ';
 }
